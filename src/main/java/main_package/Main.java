fad18774ec61f411df2ebea54eec1aa19bfe5a6f
Marcky Paulo Balaba <marckybalabamlacc@gmail.com>
2025-12-15 17:27:09 +0800

    Update Main.java

--- src/main/java/main_package/Main.java
+++ src/main/java/main_package/Main.java
@@ -1,14 +1,14 @@
-
-
-
-
+// Main class - program entry point
 package main_package;
+
 
 import service_package.TuitionManager;
 
+
 public class Main {
-    public static void main(String[] args) {
-        TuitionManager app = new TuitionManager();
-        app.startingPoint();
-    }
+public static void main(String[] args) {
+// Create system controller and start program
+TuitionManager app = new TuitionManager();
+app.startingPoint();
+}
 }
