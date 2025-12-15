764f6ab6c3dfefb1abc9cce2ff5842e4870a9331
Marcky Paulo Balaba <marckybalabamlacc@gmail.com>
2025-12-15 18:28:56 +0800

    Update Person.java

--- src/main/java/model_package/Person.java
+++ src/main/java/model_package/Person.java
@@ -1,24 +1,25 @@
-
-
-
-
+// Person abstract class - base class for people
 package model_package;
 
 public abstract class Person {
-    protected String firstName;
-    protected String lastName;
 
+    protected String firstName; // First name
+    protected String lastName; // Last name
+    
+    // Constructor for person
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-
+        // Getters
     public String getFirstName() {
-        return firstName; 
+        return firstName;
     }
     public String getLastName() {
         return lastName; 
     }
-
+    
+    // Method to be implemented by subclasses
     public abstract void displayInfo();
+    
 }
